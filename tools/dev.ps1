4ac384c9be2382c510120b5a09ddb13dd0188694
Zachary Stence <zachstence@gmail.com>
2022-09-03 20:42:41 -0500

    chore: remove tools/

--- tools/dev.ps1
+++
@@ -1,3 +0,0 @@
-# Requires nodemon to be installed (npm i -g nodemon)
-# From the root of this repo, run .\tools\dev.ps1
-nodemon --watch './**/*.go' --signal SIGTERM --exec 'go run .\cmd\main.go || exit 1'
